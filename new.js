e49650ae84096716f851c0eb047b46802ab08433
Hagar Mohammed <hagar.moh21@gmail.com>
2023-05-02 14:34:11 +0300

    Expanding cards using javascript

---
+++ new.js
@@ -0,0 +1,12 @@
+let cards = document.querySelectorAll('.img');
+
+    
+    cards.forEach((card) => {
+        card.addEventListener('click', () =>{
+            cards.forEach((card) => {
+                card.classList.remove('expand');
+            })
+            card.classList.add('expand');
+        } );
+    });
+    
